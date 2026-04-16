b35adc4096dc9ab7c17a04fec38bc3ec5c76010a
Richard Moreland <richard@mappend.com>
2026-04-16 09:07:21 -0400

    Run npm install in build.rs

--- comingle/build.rs
+++ comingle/build.rs
@@ -6,11 +6,19 @@ fn main() {
     println!("cargo:rerun-if-changed=viewer/vite.config.js");
     println!("cargo:rerun-if-changed=viewer/package.json");
 
-    let status = Command::new("npm")
+    let install_status = Command::new("npm")
+        .args(["install"])
+        .current_dir("viewer")
+        .status()
+        .expect("failed to run npm install, is Node installed?");
+
+    assert!(install_status.success(), "viewer install failed");
+
+    let build_status = Command::new("npm")
         .args(["run", "build"])
         .current_dir("viewer")
         .status()
         .expect("failed to run npm build, is Node installed?");
 
-    assert!(status.success(), "viewer build failed");
+    assert!(build_status.success(), "viewer build failed");
 }
